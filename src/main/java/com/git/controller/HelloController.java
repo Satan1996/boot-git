645fef8f1ff277b58c60b6b15c5c57f4858c2d8f
Satan1996 <52772008+Satan1996@users.noreply.github.com>
2020-02-13 01:22:08 +0800

    项目经理第一次提交项目

---
+++ src/main/java/com/git/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.git.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author Satan
+ * @Date 2020/2/131:09
+ * @Version 1.0
+ */
+@RestController
+public class HelloController {
+	@RequestMapping("info")
+	public String info(){
+		return "Hello SpringBoot Info";
+	}
+}
