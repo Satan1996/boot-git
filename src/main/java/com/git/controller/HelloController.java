5a9ea9c3b10e59664bd877ff79f00e8ec739fd8f
Satan1996 <52772008+Satan1996@users.noreply.github.com>
2020-02-13 15:49:49 +0800

    开发人员提交的

--- src/main/java/com/git/controller/HelloController.java
+++ src/main/java/com/git/controller/HelloController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("info")
 	public String info(){
-		return "Hello SpringBoot Info";
+		return "Hello SpringBoot Info 开发人员";
 	}
 	//开发人员写了一句话
 }
